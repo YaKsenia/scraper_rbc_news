5af254464e3bbccf1b7076e874660b12bc016c0a
Ksenia Yakovleva <xenia.iakovleva@gmail.com>
2019-11-08 13:28:29 +0100

    removed duplicated imports

--- scrape_rbc_articles.py
+++ scrape_rbc_articles.py
@@ -9,9 +9,6 @@ import requests
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
